63e3c8f6c263b13717a15c1fceb9e6724dd7aded
Nizul Zaim <skynightz93@gmail.com>
2017-03-09 01:19:25 +0800

    Fix Tab background & nav-list active

--- imports/client/views/templates/navigation/Navigation.vue
+++ imports/client/views/templates/navigation/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
     <scroll-bar class="flex-fill-height">
-        <router-link to="/">
+        <router-link to="/" exact>
             <nav-list class="nav-item">
                 <icon name="home"></icon> Viu Home
             </nav-list>
@@ -8,47 +8,47 @@
         <nav-list class="nav-item" :sublist="true">
             <icon name="shape-plus"></icon> Components
             <span slot="sublist">
-                <router-link to="/components/buttons">
+                <router-link to="/components/buttons" exact>
                     <nav-list class="nav-item">
                         <icon name="crop-landscape"></icon> Buttons
                     </nav-list>
                 </router-link>
-                <router-link to="/components/cards">
+                <router-link to="/components/cards" exact>
                     <nav-list class="nav-item">
                         <icon name="cards"></icon> Cards
                     </nav-list>
                 </router-link>
-                <router-link to="/components/confirmation">
+                <router-link to="/components/confirmation" exact>
                     <nav-list class="nav-item">
                         <icon name="alert-octagram"></icon> Confirmation Dialog
                     </nav-list>
                 </router-link>
-                <router-link to="/components/accordion">
+                <router-link to="/components/accordion" exact>
                     <nav-list class="nav-item">
                         <icon name="arrow-expand-all"></icon> Expansion Panel
                     </nav-list>
                 </router-link>
-                <router-link to="/components/lists">
+                <router-link to="/components/lists" exact>
                     <nav-list class="nav-item">
                         <icon name="format-list-bulleted"></icon> Lists
                     </nav-list>
                 </router-link>
-                <router-link to="/components/menu">
+                <router-link to="/components/menu" exact>
                     <nav-list class="nav-item">
                         <icon name="image-filter-none"></icon> Menu
                     </nav-list>
                 </router-link>
-                <router-link to="/components/selection-control">
+                <router-link to="/components/selection-control" exact>
                     <nav-list class="nav-item">
                         <icon name="toggle-switch"></icon> Selection Control
                     </nav-list>
                 </router-link>
-                <router-link to="/components/snackbar">
+                <router-link to="/components/snackbar" exact>
                     <nav-list class="nav-item">
                         <icon name="message"></icon> Snackbar
                     </nav-list>
                 </router-link>
-                <router-link to="/components/tooltips">
+                <router-link to="/components/tooltips" exact>
                     <nav-list class="nav-item">
                         <icon name="tooltip"></icon> Tooltips
                     </nav-list>
@@ -58,9 +58,11 @@
         <nav-list class="nav-item" :sublist="true">
             <icon name="box-shadow"></icon> Templates
             <span slot="sublist">
-                <nav-list to="/templates/sliders" class="nav-item">
-                    <icon name="fullscreen"></icon> Full Screen Slider
-                </nav-list>
+                <router-link to="/templates/sliders" exact>
+                    <nav-list  class="nav-item">
+                        <icon name="fullscreen"></icon> Full Screen Slider
+                    </nav-list>
+                </router-link>
             </span>
         </nav-list>
     </scroll-bar>
