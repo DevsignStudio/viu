da30980e5888a0b1cd74cd61b8fdfd043eb46922
Nizul Zaim <skynightz93@gmail.com>
2017-03-09 01:02:07 +0800

    Clean router config to use .routes.js file

--- client/app.js
+++ client/app.js
@@ -1,6 +1,5 @@
-import '/imports/client/router.js';
 import { Vue } from 'meteor/akryum:vue';
-import { Router, nativeScrollBehavior } from 'meteor/akryum:vue-router2';
+import router from '/imports/client/router.js';
 import AppLayout from '/imports/client/views/AppLayout.vue';
 import VueHead from 'vue-head';
 import VueHighlightJS from 'vue-highlight.js';
@@ -9,10 +8,6 @@ import 'highlight.js/styles/monokai.css';
 Vue.use(VueHighlightJS);
 Vue.use(VueHead);
 
-const router = new Router({
-    mode: 'history',
-    scrollBehavior: nativeScrollBehavior,
-});
 Meteor.startup(() => {
     new Vue({
         router: router.start(),
---
+++ client/routes/index.routes.js
@@ -0,0 +1,116 @@
+import Toolbar from '/imports/client/views/templates/toolbar/Toolbar.vue';
+import ToolbarNoBackground from '/imports/client/views/templates/toolbar/ToolbarNoBackground.vue';
+import IndexNavigation from '/imports/client/views/templates/navigation/IndexNavigation.vue';
+import GlobalNavigation from '/imports/client/views/templates/navigation/GlobalNavigation.vue';
+import Index from '/imports/client/views/public/Index.vue';
+import Lists from '/imports/client/views/public/components/Lists.vue';
+import SelectionControl from '/imports/client/views/public/components/SelectionControl.vue';
+import Accordions from '/imports/client/views/public/components/Accordions.vue';
+import ComponentCards from '/imports/client/views/public/components/Card.vue';
+import ComponentButton from '/imports/client/views/public/components/Buttons.vue';
+import ComponentTooltips from '/imports/client/views/public/components/Tooltips.vue';
+import ComponentSnackbar from '/imports/client/views/public/components/Snackbar.vue';
+import ComponentConfirmation from '/imports/client/views/public/components/Confirmation.vue';
+import ComponentMenu from '/imports/client/views/public/components/Menu.vue';
+import TemplateSlider from '/imports/client/views/public/templates/Sliders.vue';
+
+export default [{
+    path: "/",
+    name: "Index",
+    meta: { fixToolbar: true, pageTitle: "Viu Doc" },
+    components: {
+        default: Index,
+        toolbar: Toolbar,
+        navigation: IndexNavigation,
+    }
+}, {
+    path: "/components/lists",
+    name: "Lists",
+    meta: { fixToolbar: true, pageTitle: "Lists" },
+    components: {
+        default: Lists,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/selection-control",
+    name: "SelectionCOntrol",
+    meta: { fixToolbar: true, pageTitle: "Selection Control" },
+    components: {
+        default: SelectionControl,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/accordion",
+    name: "Accordions",
+    meta: { fixToolbar: true, pageTitle: "Expansion Panels" },
+    components: {
+        default: Accordions,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/cards",
+    name: "Cards",
+    meta: { fixToolbar: true, pageTitle: "Cards" },
+    components: {
+        default: ComponentCards,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/buttons",
+    name: "Buttons",
+    meta: { fixToolbar: true, pageTitle: "Buttons" },
+    components: {
+        default: ComponentButton,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/tooltips",
+    name: "Tooltips",
+    meta: { fixToolbar: true, pageTitle: "Tooltips" },
+    components: {
+        default: ComponentTooltips,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/snackbar",
+    name: "Snackbar",
+    meta: { fixToolbar: true, pageTitle: "Snackbar" },
+    components: {
+        default: ComponentSnackbar,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/confirmation",
+    name: "Confirmation",
+    meta: { fixToolbar: true, pageTitle: "Confirmation" },
+    components: {
+        default: ComponentConfirmation,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/components/menu",
+    name: "Menu",
+    meta: { fixToolbar: true, pageTitle: "Menu" },
+    components: {
+        default: ComponentMenu,
+        toolbar: Toolbar,
+        navigation: GlobalNavigation,
+    }
+}, {
+    path: "/templates/sliders",
+    name: "Slider",
+    meta: { fixToolbar: true, pageTitle: "Slider" },
+    components: {
+        default: TemplateSlider,
+        toolbar: ToolbarNoBackground,
+        navigation: IndexNavigation,
+    }
+}, ];
