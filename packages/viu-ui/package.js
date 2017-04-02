4c3610275440f399aa2a29b91d41dbfd8178705d
Nizul Zaim <mnizulzaim@gmail.com>
2017-04-03 04:54:07 +0800

    bump package 0.1.2

--- packages/viu-ui/package.js
+++ packages/viu-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'devsignstudio:viu-ui',
-    version: '0.1.0',
+    version: '0.1.1',
     // Brief, one-line summary of the package.
     summary: 'Material Design vue-meteor package',
     // URL to the Git repository containing the source code for this package.
