072f2e29b1143664862a9096091859c50c276a74
Nizul Zaim <mnizulzaim@gmail.com>
2017-04-26 17:42:26 +0800

    Fix temporary for v-depth & related issue
    Bump package

--- packages/viu-ui/client/directives/v-depth.js
+++ packages/viu-ui/client/directives/v-depth.js
@@ -1,6 +1,7 @@
 export default function(el, binding, vNode) {
     let $el = $(el);
-    if (binding.value) {
+    if (binding.value !== undefined && binding.value !== null) {
+        $el.bind(this).removeClass('z-depth z-value-0 z-value-1 z-value-2 z-value-3 z-value-4 z-value-5');
         $el.bind(this).addClass('z-depth z-value-' + binding.value);
     }
 };
--- packages/viu-ui/package.js
+++ packages/viu-ui/package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: 'devsignstudio:viu-ui',
-    version: '0.1.1',
+    version: '0.1.2',
     // Brief, one-line summary of the package.
     summary: 'Material Design vue-meteor package',
     // URL to the Git repository containing the source code for this package.
